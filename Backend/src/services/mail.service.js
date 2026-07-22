77dff1bff89b1caf363bf92a31e9838c41a2541c
akash0528 <akashdivakar852@.com>
2026-07-22 18:16:50 +0530

    updating

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,37 +1,42 @@
-import Mailjet from "node-mailjet";
+import dotenv from "dotenv";
+dotenv.config();
 
-const mailjet = Mailjet.apiConnect(
-    process.env.MAILJET_API_KEY,
-    process.env.MAILJET_SECRET_KEY
-);
+import nodemailer from "nodemailer";
 
-export async function sendEmail({ to, subject, html, text }) {
-    try {
-        const result = await mailjet
-            .post("send", { version: "v3.1" })
-            .request({
-                Messages: [
-                    {
-                        From: {
-                            Email: "gk9605873@gmail.com", // apna verified sender email
-                            Name: "Perplexity",
-                        },
-                        To: [
-                            {
-                                Email: to,
-                            },
-                        ],
-                        Subject: subject,
-                        HTMLPart: html,
-                        TextPart: text,
-                    },
-                ],
-            });
+export const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls:{
+    rejectUnauthorized:false
+  }
+});
 
-        console.log("Email sent:", result.body);
-        return result.body;
-    } catch (error) {
-        console.error("Email sending failed:", error);
-        throw error;
-    }
-}
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("VERIFY ERROR => ", error);
+  } else {
+    console.log("Email server is ready");
+  }
+});
+
+export const sendEmail = async (to, subject, text, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: "akashdivakar1221@gmail.com",
+      to,
+      subject,
+      text,
+      html,
+    });
+
+    console.log("Message sent:", info.messageId);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
